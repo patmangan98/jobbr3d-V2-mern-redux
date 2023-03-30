6fb5bcb9409b9360a69efd5283e24e57db80e0b8
Patrick Mangan <patrickmangan@Patricks-Mini.lan>
2023-03-30 14:46:58 -0400

    login now works, and the routes for customers have been adjusted

--- routes/api/customer.js
+++ routes/api/customer.js
@@ -1,1 +1,8 @@
 const express = require("express")
+const router = express.Router()
+const customerCtrl = require("../../controllers/api/customer")
+
+router.post("/new", customerCtrl.createCustomer)
+router.get("/all", customerCtrl.indexCustomers)
+
+module.exports = router
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const port = process.env.PORT || 3001;
 
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'))
+app.use("/api/users/login", require("./routes/api/users"));
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
