6339ee068c3b0a78be5618e32dbfd881ef631663
Patrick Mangan <patrickmangan@Patricks-Mini.lan>
2023-04-11 14:30:42 -0400

    moved the update customer form into its own compenent

--- src/pages/components/Customer/CustomerCard.js
+++ src/pages/components/Customer/CustomerCard.js
@@ -1,42 +1,43 @@
 import { deleteCustomer, indexCustomers, updateOneCustomer } from "../../../utilities/customer-api"
 import { useState } from "react"
+import UpdateCustomerForm from "./UpdateCustomerForm"
 
 export default function CustomerCard ({customer, index, setCustomerArr}) {
 
     // console.log(customer.owner)
 
-    const [updateCustomer, setUpdateCustomer] = useState({
-        // _id: `${customer._id}`,
-        firstName: `${customer.firstName}`,
-        lastName: `${customer.lastName}`,
-        contact: `${customer.contact}`,
-        description: `${customer.description}`,
-    })
+    // const [updateCustomer, setUpdateCustomer] = useState({
+    //     // _id: `${customer._id}`,
+    //     firstName: `${customer.firstName}`,
+    //     lastName: `${customer.lastName}`,
+    //     contact: `${customer.contact}`,
+    //     description: `${customer.description}`,
+    // })
 
-    console.log(customer._id)
+    // console.log(customer._id)
 
-    function handleChange (event) {
-        setUpdateCustomer({
-            ...updateCustomer,
-            // _id : `${customer._id}`,
-            [event.target.name]: event.target.value,
-        })
-    }
+    // function handleChange (event) {
+    //     setUpdateCustomer({
+    //         ...updateCustomer,
+    //         // _id : `${customer._id}`,
+    //         [event.target.name]: event.target.value,
+    //     })
+    // }
 
 
-    async function handleUpdateCustomer (event) {
-        event.preventDefault()
-        try {
-            // const custId = customer.owner._id 
-            // console.log(custId)
-            const formData = {...updateCustomer}
-            console.log(formData)
-            await updateOneCustomer(formData, customer._id)
+    // async function handleUpdateCustomer (event) {
+    //     event.preventDefault()
+    //     try {
+    //         // const custId = customer.owner._id 
+    //         // console.log(custId)
+    //         const formData = {...updateCustomer}
+    //         console.log(formData)
+    //         await updateOneCustomer(formData, customer._id)
 
-        } catch (error) {
-            console.error(error)
-        }
-    }
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // }
 
 
     function handleDeleteCustomer(event) {
@@ -61,8 +62,10 @@ export default function CustomerCard ({customer, index, setCustomerArr}) {
         >Delete Customer</button>
         <button>Show Details</button>
 
+        <UpdateCustomerForm customer={customer}/>
 
-        <form>
+
+        {/* <form>
         <input
             type="text"
             name="firstName"
@@ -92,7 +95,7 @@ export default function CustomerCard ({customer, index, setCustomerArr}) {
         
         >Update Customer</button>
 
-        </form>
+        </form> */}
         </>
     )
 } 
--- src/pages/components/Customer/UpdateCustomerForm.js
+++ src/pages/components/Customer/UpdateCustomerForm.js
@@ -1,3 +1,73 @@
-export default function UpdateCustomerForm () {
-    return
+import { useState } from "react"
+import { updateOneCustomer } from "../../../utilities/customer-api"
+export default function UpdateCustomerForm ({customer, setCustomerArr}) {
+    const [updateCustomer, setUpdateCustomer] = useState({
+        // _id: `${customer._id}`,
+        firstName: `${customer.firstName}`,
+        lastName: `${customer.lastName}`,
+        contact: `${customer.contact}`,
+        description: `${customer.description}`,
+    })
+
+    console.log(customer._id)
+
+    function handleChange (event) {
+        setUpdateCustomer({
+            ...updateCustomer,
+            // _id : `${customer._id}`,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+
+    async function handleUpdateCustomer (event) {
+        event.preventDefault()
+        try {
+            // const custId = customer.owner._id 
+            // console.log(custId)
+            const formData = {...updateCustomer}
+            console.log(formData)
+            await updateOneCustomer(formData, customer._id)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return (
+        <>
+         <form>
+        <input
+            type="text"
+            name="firstName"
+            defaultValue={customer.firstName}
+            onChange={handleChange}
+        />
+        <input
+            type="text"
+            name="lastName"
+            defaultValue={customer.lastName}
+            onChange={handleChange}
+        />
+        <input
+            type="text"
+            name="contact"
+            defaultValue={customer.contact}
+            onChange={handleChange}
+        />
+        <input
+            type="text"
+            name="description"
+            defaultValue={customer.description}
+            onChange={handleChange}
+        />
+        <button
+            onClick ={handleUpdateCustomer}
+        
+        >Update Customer</button>
+
+        </form>
+        </>
+
+        
+    )
 }
