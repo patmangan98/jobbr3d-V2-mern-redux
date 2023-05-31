4e2dcffb1ba834474a36b8e115eb6cb8590887ff
Patrick Mangan <patrickmangan@Patricks-Mini.lan>
2023-05-31 15:14:16 -0400

    bug fixed with create prints, now moving to the front end

--- controllers/api/print.js
+++ controllers/api/print.js
@@ -2,22 +2,21 @@ const customer = require('../../models/customer')
 const Customer = require('../../models/customer')
 const Print = require('../../models/print')
 //create a print 
+// function createPrint(req, res, next) {
 function createPrint(req, res, next) {
     
-    //console.log(customerId)
-   
     const print = req.body.print
-    const customerId = req.params
+    const customerId = req.body.print.customerId
     console.log(customerId)
-    Customer.find({_id : customerId})
+    Customer.findById(customerId)
         .then((customer) => {
-        
             customer.prints.push(print)
             return customer.save()
         })
-        .then((customer) => res.status(200).json({ customer: customer }))
+        .then((customer) => res.status(204).json({ customer: customer }))
         .catch(next)
 }
+
 
 // index all prints 
 function indexAllPrints(req, res, next) {
@@ -37,7 +36,7 @@ function indexAllPrints(req, res, next) {
         .then((prints) => {
             res.status(200).json({ prints: prints })
         })
-        .catch(next) 
+        .catch(next)
 }
 
 module.exports = {
