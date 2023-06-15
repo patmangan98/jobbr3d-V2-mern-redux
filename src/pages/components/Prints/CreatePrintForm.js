6c7925fc9f8e8b35c760ab0a1c1debe7491c3f95
Patrick Mangan <patrickmangan@Patricks-Mini.lan>
2023-06-15 10:57:15 -0400

    getting the select drop down to work

--- src/pages/components/Prints/CreatePrintForm.js
+++ src/pages/components/Prints/CreatePrintForm.js
@@ -2,76 +2,103 @@ import { useState, useEffect } from "react"
 import { indexCustomers } from "../../../utilities/customer-api"
 
 
+
 export default function CreatePrintForm() {
 
-    const [options, SetOptions] = useState([])
+    const [options, setOptions] = useState([])
+
 
     useEffect(() => {
         indexCustomers()
             .then((res) => res.json())
-            .then((resData) => SetOptions(resData.customers))
+            .then((resData) => setOptions(resData.customers))
     }, [])
 
 
+    const [selection, SetSelection] = useState('')
 
     const optionsMap = options.map((customer) => (
         <option
-            value={customer._id}
-            key={customer._id}>
+            name= {customer._id}
+            value ={customer._id}
+            key={customer._id}
+            >
             {customer.firstName} {customer.lastName}
         </option>
 
     ))
 
+    const [newPrintData, setNewPrintData] = useState({
+        weight: "",
+        hoursToPrint: "",
+        description: "",
+        isDone: "",
+        customerId :`${selection}`,
+    })
 
+    function handleChange(event) {
+      
+        setNewPrintData({
+            ...newPrintData,
+            [event.target.name]: event.target.value,
+            customerId:`${selection}`,    
+        })
+        
+    }
+
+    function handleSelectionChange (event) {
+        if (event.target.value != "") {
+            SetSelection(event.target.value)
+        }
+       
+    }
+
+
+     console.log(newPrintData)
+     console.log(selection)
     return (
 
-
-
         <>
-
-
-
             <form>
-                <select>
+            <label>Select Customer</label>
+                <select onChange={handleSelectionChange}>
+                   <option>Please select a customer</option>
                     {optionsMap}
                 </select>
+                <label> Weight of the part</label>
                 <input
-                    type="text"
-                    name="description"
-                    placeholder="Customer First Name"
-                // value={customer.firstName}
-                // onChange={handleChange}
-                />
-                <label>Description</label>
-
-                <input
-                    type="text"
-                    name="hoursToPrint"
-                    placeholder="Customer Last Name"
-                // value={customer.lastName}
-                // onChange={handleChange}
+                    type="number"
+                    name="weight"
+                    placeholder="weight of print"
+                    value={newPrintData.weight}
+                    onChange={handleChange}
                 />
                 <label>Hours to Print</label>
-
+                <input
+                    type="number"
+                    name="hoursToPrint"
+                    placeholder="hours to print"
+                    value={newPrintData.hoursToPrint}
+                    onChange={handleChange}
+                />
+                <label>Description</label>
+                 <input
+                    type="text"
+                    name="description"
+                    placeholder="Notes About Customer Job"
+                    value={newPrintData.description}
+                    onChange={handleChange}
+                />
+                <label>Is it completed?</label>
                 <input
                     type="text"
                     name="isDone"
-                    placeholder="Customer Contact Information"
-                // value={customer.contact}
-                // onChange={handleChange}
+                    placeholder="is customer job completed"
+                    value={newPrintData.isDone}
+                    onChange={handleChange}
                 />
-                <label>Is it completed?</label>
-
-                <input
-                    type="text"
-                    name="weight"
-                    placeholder="Notes About Customer Job"
-                // value={customer.description}
-                // onChange={handleChange}
-                />
-                <label>Weight</label>
-
+               
+               
                 <button
                     type="submit"
                 // onClick = {handleSubmit}
