7684804710b1bd85f3db0f75b80c7bf8c9098871
Patrick Mangan <patrickmangan@Patricks-Mini.attlocal.net>
2023-07-31 13:34:17 -0400

    cleaning up auth page functionality

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,12 +1,57 @@
 import SignUpForm from "../components/SignUpForm/SignUpForm";
 import LoginForm from '../components/LoginForm/LoginForm'
+import {useState} from 'react'
 
 export default function AuthPage({ setUser }) {
+
+const [toggleForms, setToggleForms] = useState(true)
+
+let buttonText = 'Login'
+let userMsg = "Log-in to Jobbr3d"
+
+function changeButtonText (arg) {
+	if (arg === false) {
+		buttonText = "Sign-Up"
+		userMsg = "Log-in to Jobbr3d"
+	}
+
+	if (arg === true) {
+		buttonText = "Login"
+		userMsg = "Sign-up for Jobber3d"
+	}
+}
+
+
+function toggleVisible () {
+	setToggleForms(!toggleForms)
+}
+
+changeButtonText(toggleForms)
+// let btnText = "Sign-Up"
+
+// function changeButtonText () {
+// 	if (buttonText === false) {
+// 		btnText = "Login"
+// 	}
+
+// 	if (buttonText = true) {
+// 		btnText = "Sign-up"
+// 	}
+// }
+// changeButtonText()
+
+console.log(toggleForms)
+
 	return (
+
+
 		<>
-			<h2>AuthPage</h2>
-            <SignUpForm setUser={setUser} />
-			<LoginForm setUser={setUser} />
+			<h2>{userMsg}</h2>
+			<button onClick={toggleVisible}>{buttonText}</button>
+
+			{toggleForms ? <SignUpForm setUser={setUser}/> : <LoginForm setUser={setUser}/>}
+            {/* <SignUpForm setUser={setUser} />
+			<LoginForm setUser={setUser} /> */}
 		</>
 	)
 }
